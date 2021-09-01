c37f17c6f55d2d14378535bf8c4800c71748301c
MDz1985 <m_d@tut.by>
2021-09-01 15:12:00 +0300

    feat: mostly done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,37 @@
 class GuessingGame {
-    constructor() {}
+        
+        
+    constructor(number) {
+      this.min;
+      this.max;
+      this.guessNumber;
+        
+    }
 
     setRange(min, max) {
+      this.min = min;
+      this.max = max;
+      
 
     }
 
     guess() {
+         
+          return this.guessNumber = Math.floor((this.min+this.max)/2);
+         
+        
+        
 
     }
 
     lower() {
-
+      
+       this.max = this.guessNumber+1;
     }
 
     greater() {
-
+      
+        this.min = this.guessNumber;
     }
 }
 
